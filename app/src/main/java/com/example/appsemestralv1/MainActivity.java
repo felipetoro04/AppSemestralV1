8269f7a7ba50f678996b342cf636d5a1f96dad7a
felipetoro04 <felipe.toro2020@twk.cl>
2021-05-03 17:10:36 -0400

    First Oficial commit

--- app/src/main/java/com/example/appsemestralv1/MainActivity.java
+++ app/src/main/java/com/example/appsemestralv1/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.appsemestralv1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity  implements DialogLogin.DialogLoginListener{
+    private Button loginbutton;
+    private TextView textView_Email;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView_Email = findViewById(R.id.textView_email);
+
+
+        loginbutton = (Button) findViewById(R.id.LoginButton);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+
+
+
+            }
+        });
+
     }
-}
+    public void openDialog(){
+        DialogLogin dialogLogin = new DialogLogin();
+        dialogLogin.show(getSupportFragmentManager(),"dialog Login");
+
+
+    }
+    @Override
+    public  void  applyTexts(String email){
+        textView_Email.setText(email);
+
+    }
+
+
+    }
