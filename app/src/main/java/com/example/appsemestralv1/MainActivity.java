c34851706a99d5761761ac6225bacc9e292931c7
Daniiparedes <Basilio.paredes2020@twk.cl>
2021-06-28 20:36:42 -0400

    HistorialLayoutbackbutton

--- app/src/main/java/com/example/appsemestralv1/MainActivity.java
+++ app/src/main/java/com/example/appsemestralv1/MainActivity.java
@@ -7,7 +7,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -16,6 +21,7 @@ import android.media.MediaPlayer;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -54,6 +60,7 @@ public class MainActivity extends AppCompatActivity  implements  View.OnClickLis
     public Button btn_Track;
     public User user;
     public Button callPolice;
+    private static final int PERMISSION_SEND_SMS = 123;
     daoUser dao;
     int userId = -1;
     MediaPlayer mp;
@@ -76,6 +83,8 @@ String coordenadaText="";
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         Coordenadas = findViewById(R.id.TVcoordenadas);
         textView_Email = findViewById(R.id.textView_email);
+
+
 
         callPolice = (Button) findViewById(R.id.CallPolice);
         callPolice.setOnClickListener(this);
@@ -124,9 +133,48 @@ String coordenadaText="";
 
         }
 
-
-
     }
+
+    private void sendSms(String address, String message) {
+
+        try {
+                String SENT = "SMS_SENT";
+
+                PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+
+                registerReceiver(new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context arg0, Intent arg1) {
+                        int resultCode = getResultCode();
+                        switch (resultCode) {
+                            case Activity.RESULT_OK:
+                                Toast.makeText(getBaseContext(), "SMS sent", Toast.LENGTH_LONG).show();
+                                break;
+                            case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                                Toast.makeText(getBaseContext(), "Generic failure", Toast.LENGTH_LONG).show();
+                                break;
+                            case SmsManager.RESULT_ERROR_NO_SERVICE:
+                                Toast.makeText(getBaseContext(), "No service", Toast.LENGTH_LONG).show();
+                                break;
+                            case SmsManager.RESULT_ERROR_NULL_PDU:
+                                Toast.makeText(getBaseContext(), "Null PDU", Toast.LENGTH_LONG).show();
+                                break;
+                            case SmsManager.RESULT_ERROR_RADIO_OFF:
+                                Toast.makeText(getBaseContext(), "Radio off", Toast.LENGTH_LONG).show();
+                                break;
+                        }
+                    }
+                }, new IntentFilter(SENT));
+
+                SmsManager smsMgr = SmsManager.getDefault();
+                smsMgr.sendTextMessage(address, null, message, sentPI, null);
+            } catch (Exception e) {
+                Toast.makeText(this, e.getMessage() + "!\n" + "Failed to send SMS", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -135,7 +183,16 @@ String coordenadaText="";
         } else {
             askLocationPermission();
         }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    PERMISSION_SEND_SMS);
+
+        }
     }
+
     private void getLastLocation() {
         Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
 
@@ -162,6 +219,8 @@ String coordenadaText="";
                         };
                 }
             }
+
+
         });
 
         locationTask.addOnFailureListener(new OnFailureListener() {
@@ -247,13 +306,20 @@ String coordenadaText="";
                 break;
 
             case R.id.CallFamily:
-                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + dao.getPhone())));
+                //startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + dao.getPhone())));
                 getLastLocation();
+                String location = "NECESITO AYUDA! https://maps.google.com/maps?ll=" + dao2.getLastLocation().replace(" ","") + ",&amp;z=16&amp;t=m&amp;hl=en-US&amp;gl=US&amp;mapclient=apiv3";
+                String number = "56" + dao.getPhone();
+                sendSms(number, location);
+                Toast.makeText(this, "Mensaje Enviado", Toast.LENGTH_SHORT).show();
+
                 break;
 
             case R.id.CallPolice:
                 startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "133")));
                 break;
+
+
         }
     };
 
--- app/src/main/java/com/example/appsemestralv1/daoUbication.java
+++ app/src/main/java/com/example/appsemestralv1/daoUbication.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class daoUbication {
     Context context; //provee una conection entre ando=rid y nuestro proyecto
@@ -13,7 +14,7 @@ public class daoUbication {
     ArrayList<Ubication> list; // declaramos array list
     SQLiteDatabase conn; //Acceder a los metodos de trabajos sql
     String bd = "ExampleApp";//declaracion de DDBB
-    String table = "create table if not exists ubications(id integer primary key autoincrement, coordenadas text)";
+    String table = "create table if not exists ubications(id integer primary key autoincrement, coordenadas text, fecha Date)";
 
 
     public daoUbication(Context context) {
@@ -28,6 +29,8 @@ public class daoUbication {
     public void createUbication(Ubication ubication) {
         ContentValues cv = new ContentValues();   //tipo de lista que contendra varios valores
         cv.put("coordenadas", ubication.getCoordenadas());// asigname tag con el valor que nos retorne el objeto
+        cv.put("fecha", String.valueOf(Calendar.getInstance().getTime()));
+
        conn.insert("ubications", null, cv) ;
     }
 
@@ -37,7 +40,7 @@ public class daoUbication {
         ArrayList<Ubication> list = new ArrayList<Ubication>();
         list.clear();//limpiar lista
 
-        Cursor cursor = conn.rawQuery("SELECT * FROM ubications", null);//este cursor retornara los resultado de la query
+        Cursor cursor = conn.rawQuery("SELECT * FROM ubications ORDER BY fecha DESC", null);//este cursor retornara los resultado de la query
 
         if (cursor != null) {
             if (cursor.moveToFirst()) {
@@ -64,7 +67,10 @@ public class daoUbication {
         }
         return null;
     }
-
+    public String getLastLocation() {
+        String location = getUbications().get(0).Coordenadas;
+        return location;
+    }
 
 }
 
